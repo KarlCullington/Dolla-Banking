716480f19ba1f363689ca5a73fcbc558b71eec10
KPC-Web <75866826+KPC-Web@users.noreply.github.com>
2021-09-09 06:56:14 +0200

    customised page

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,110 +1,128 @@
-import React from "react";
-import { animateScroll as scroll } from "react-scroll";
-
+import React from 'react';
+import { animateScroll as scroll } from 'react-scroll';
 import {
-  FaFacebook,
-  FaInstagram,
-  FaYoutube,
-  FaTwitter,
-  FaLinkedin,
-} from "react-icons/fa";
-
+    FaFacebook,
+    FaInstagram,
+    FaYoutube,
+    FaTwitter,
+    FaGithub,
+    FaLinkedin,
+} from 'react-icons/fa';
 import {
-  FooterContainer,
-  FooterWrapper,
-  FooterLinkContainer,
-  FooterLinkWrapper,
-  FooterLinkItems,
-  FooterLinkTitle,
-  FooterLink,
-  SocialMedia,
-  SocialMediaWrap,
-  SocialLogo,
-  WebsiteRights,
-  SocialIcons,
-  SocialIconLink,
-} from "./FooterElements";
+    FooterContainer,
+    FooterWrap,
+    FooterLinksContainer,
+    FooterLinksWrapper,
+    FooterLinkItems,
+    FooterLinkTitle,
+    FooterLink,
+    SocialMedia,
+    SocialMediaWrap,
+    WebsiteRights,
+    SocialLogo,
+    SocialIcons,
+    SocialIconLink,
+} from './FooterElements';
 
 const Footer = () => {
-  const toggleHome = () => {
-    scroll.scrollToTop();
-  };
+    const toggleHome = () => {
+        scroll.scrollToTop();
+    };
 
-  return (
-    <FooterContainer>
-      <FooterWrapper>
-        <FooterLinkContainer>
-          <FooterLinkWrapper>
-            <FooterLinkItems>
-              <FooterLinkTitle>About Us</FooterLinkTitle>
-              <FooterLink to="/signin">How it Works</FooterLink>
-              <FooterLink to="/signin">Testimonials</FooterLink>
-              <FooterLink to="/signin">Careers</FooterLink>
-              <FooterLink to="/signin">Investors</FooterLink>
-              <FooterLink to="/signin">Terms of services</FooterLink>
-            </FooterLinkItems>
+    return (
+        <FooterContainer>
+            <FooterWrap>
+                <FooterLinksContainer>
+                    <FooterLinksWrapper>
+                        <FooterLinkItems>
+                            <FooterLinkTitle>About Us</FooterLinkTitle>
+                            <FooterLink to='/signin'>How it works</FooterLink>
+                            <FooterLink to='/'>Testimonials</FooterLink>
+                            <FooterLink to='/'>Careers</FooterLink>
+                            <FooterLink to='/'>Investors</FooterLink>
+                            <FooterLink to='/'>Terms of Service</FooterLink>
+                        </FooterLinkItems>
+                        <FooterLinkItems>
+                            <FooterLinkTitle>Contact Us</FooterLinkTitle>
+                            <FooterLink to='/'>Contact</FooterLink>
+                            <FooterLink to='/'>Support</FooterLink>
+                            <FooterLink to='/'>Destination</FooterLink>
+                            <FooterLink to='/'>Sponsorships</FooterLink>
+                        </FooterLinkItems>
+                    </FooterLinksWrapper>
 
-            <FooterLinkItems>
-              <FooterLinkTitle>Videos</FooterLinkTitle>
-              <FooterLink to="/signin">Submit video</FooterLink>
-              <FooterLink to="/signin">Ambassadors</FooterLink>
-              <FooterLink to="/signin">Agency</FooterLink>
-              <FooterLink to="/signin">Influencer</FooterLink>
-            </FooterLinkItems>
-          </FooterLinkWrapper>
-
-          <FooterLinkWrapper>
-            <FooterLinkItems>
-              <FooterLinkTitle>Contact Us</FooterLinkTitle>
-              <FooterLink to="/signin">Contact</FooterLink>
-              <FooterLink to="/signin">Support</FooterLink>
-              <FooterLink to="/signin">Destinations</FooterLink>
-              <FooterLink to="/signin">Sponsorships</FooterLink>
-            </FooterLinkItems>
-
-            <FooterLinkItems>
-              <FooterLinkTitle>Social Media</FooterLinkTitle>
-              <FooterLink to="/signin">Instagram</FooterLink>
-              <FooterLink to="/signin">Facebook</FooterLink>
-              <FooterLink to="/signin">Youtube</FooterLink>
-              <FooterLink to="/signin">Twitter</FooterLink>
-              <FooterLink to="/signin">Linkedin</FooterLink>
-            </FooterLinkItems>
-          </FooterLinkWrapper>
-        </FooterLinkContainer>
-
-        <SocialMedia>
-          <SocialMediaWrap>
-            <SocialLogo to="/" onClick={toggleHome}>
-              dolla
-            </SocialLogo>
-            <WebsiteRights>
-              Dolla &copy; {new Date().getFullYear()} All rights reserved
-            </WebsiteRights>
-            <SocialIcons>
-
-              <SocialIconLink href="/" target="_blank" aria-label="Facebook">
-                <FaFacebook />
-              </SocialIconLink>
-              <SocialIconLink href="/" target="_blank" aria-label="Instagram">
-                <FaInstagram />
-              </SocialIconLink>
-              <SocialIconLink href="/" target="_blank" aria-label="Twitter">
-                <FaTwitter />
-              </SocialIconLink>
-              <SocialIconLink href="/" target="_blank" aria-label="Youtube">
-                <FaYoutube />
-              </SocialIconLink>
-              <SocialIconLink href="/" target="_blank" aria-label="Linkedin">
-                <FaLinkedin />
-              </SocialIconLink>
-
-            </SocialIcons>
-          </SocialMediaWrap>
-        </SocialMedia>
-      </FooterWrapper>
-    </FooterContainer>
-  );
+                    <FooterLinksWrapper>
+                        <FooterLinkItems>
+                            <FooterLinkTitle>Videos</FooterLinkTitle>
+                            <FooterLink to='/'>Submit Videos</FooterLink>
+                            <FooterLink to='/'>Ambassadors</FooterLink>
+                            <FooterLink to='/'>Agency</FooterLink>
+                            <FooterLink to='/'>Influencer</FooterLink>
+                        </FooterLinkItems>
+                        <FooterLinkItems>
+                            <FooterLinkTitle>Something Else</FooterLinkTitle>
+                            <FooterLink to='/'>I have</FooterLink>
+                            <FooterLink to='/'>Absolutly</FooterLink>
+                            <FooterLink to='/'>No idea what to </FooterLink>
+                            <FooterLink to='/'>Link to, ideally</FooterLink>
+                            <FooterLink to='/'>
+                                Something within this
+                            </FooterLink>
+                        </FooterLinkItems>
+                    </FooterLinksWrapper>
+                </FooterLinksContainer>
+                <SocialMedia>
+                    <SocialMediaWrap>
+                        <SocialLogo to='/' onClick={toggleHome}>
+                            dolla
+                        </SocialLogo>
+                        <WebsiteRights>
+                            dolla © {new Date().getFullYear()} All rights
+                            reserved.
+                        </WebsiteRights>
+                        <SocialIcons>
+                            <SocialIconLink
+                                href='/'
+                                target='_blank'
+                                aria-label='Facebook'>
+                                <FaFacebook />
+                            </SocialIconLink>
+                            <SocialIconLink
+                                href='//www.facebook.com'
+                                target='_blank'
+                                aria-label='Instagram'>
+                                <FaInstagram />
+                            </SocialIconLink>
+                            <SocialIconLink
+                                href='/'
+                                target='_blank'
+                                aria-label='Youtube'>
+                                <FaYoutube />
+                            </SocialIconLink>
+                            <SocialIconLink
+                                href='/'
+                                target='_blank'
+                                aria-label='Twitter'>
+                                <FaTwitter />
+                            </SocialIconLink>
+                            <SocialIconLink
+                                href='/'
+                                target='_blank'
+                                aria-label='Github'>
+                                <FaGithub />
+                            </SocialIconLink>
+                            <SocialIconLink
+                                href='/'
+                                target='_blank'
+                                aria-label='Linkedin'>
+                                <FaLinkedin />
+                            </SocialIconLink>
+                        </SocialIcons>
+                    </SocialMediaWrap>
+                </SocialMedia>
+            </FooterWrap>
+        </FooterContainer>
+    );
 };
 
 export default Footer;
--- src/components/Services/index.js
+++ src/components/Services/index.js
@@ -1,50 +1,63 @@
-import React from "react";
-import icon1 from '../../images/svg-1.svg'
-import icon2 from '../../images/svg-2.svg'
-import icon3 from '../../images/svg-3.svg'
+import React from 'react';
+import icon1 from '../../images/svg-1.svg';
+import icon2 from '../../images/svg-2.svg';
+import icon3 from '../../images/svg-3.svg';
+import icon4 from '../../images/svg-4.svg';
 
 import {
-  ServicesContainer,
-  ServicesH1,
-  ServicesWrapper,
-  ServicesCard,
-  ServicesH2,
-  ServicesIcon,
-  ServicesP,
-} from "./ServicesElements";
+    ServicesContainer,
+    ServicesH1,
+    ServicesWrapper,
+    ServicesCard,
+    ServicesH2,
+    ServicesIcon,
+    ServicesP,
+} from './ServicesElements';
 
 const Services = () => {
-  return (
-    <ServicesContainer id="services">
-      <ServicesH1>Our Services</ServicesH1>
+    return (
+        <ServicesContainer id='services'>
+            <ServicesH1>Our Services</ServicesH1>
 
-      <ServicesWrapper>
-        <ServicesCard>
-          <ServicesIcon src={icon1} />
-          <ServicesH2>Reduce Expenses</ServicesH2>
-          <ServicesP>
-            We help reduce your fees and increase your overall revenue.
-          </ServicesP>
-        </ServicesCard>
+            <ServicesWrapper>
+                <ServicesCard>
+                    <ServicesIcon src={icon1} />
+                    <ServicesH2>Reduce Expenses</ServicesH2>
+                    <ServicesP>
+                        We help reduce your fees and increase your overall
+                        revenue.
+                    </ServicesP>
+                </ServicesCard>
 
-        <ServicesCard>
-          <ServicesIcon src={icon2} />
-          <ServicesH2>Virtual Offices</ServicesH2>
-          <ServicesP>
-            You can access our platform online anywhere in the world.
-          </ServicesP>
-        </ServicesCard>
+                <ServicesCard>
+                    <ServicesIcon src={icon2} />
+                    <ServicesH2>Virtual Offices</ServicesH2>
+                    <ServicesP>
+                        You can access our platform online anywhere in the
+                        world.
+                    </ServicesP>
+                </ServicesCard>
 
-        <ServicesCard>
-          <ServicesIcon src={icon3} />
-          <ServicesH2>Premium Benefits</ServicesH2>
-          <ServicesP>
-            Unlock our special mebership card that returns 5% cash back.
-          </ServicesP>
-        </ServicesCard>
-      </ServicesWrapper>
-    </ServicesContainer>
-  );
+                <ServicesCard>
+                    <ServicesIcon src={icon3} />
+                    <ServicesH2>Premium Benefits</ServicesH2>
+                    <ServicesP>
+                        Unlock our special mebership card that returns 5% cash
+                        back.
+                    </ServicesP>
+                </ServicesCard>
+
+                <ServicesCard>
+                    <ServicesIcon src={icon4} />
+                    <ServicesH2>Premium Benefits</ServicesH2>
+                    <ServicesP>
+                        Unlock our special mebership card that returns 5% cash
+                        back.
+                    </ServicesP>
+                </ServicesCard>
+            </ServicesWrapper>
+        </ServicesContainer>
+    );
 };
 
 export default Services;
